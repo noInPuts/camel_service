dd82c6a600b998f65b4c4e32a98991bf76bf6dd4
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-10-29 20:43:08 +0100

    Created Account- & Restaurant service java init

--- restaurant_service/src/main/java/cphbusiness/noinputs/main/RestaurantServiceApplication.java
+++ restaurant_service/src/main/java/cphbusiness/noinputs/main/RestaurantServiceApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebserviceApplication {
+
+public class RestaurantServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebserviceApplication.class, args);
+        SpringApplication.run(RestaurantServiceApplication.class, args);
     }
 
 }
