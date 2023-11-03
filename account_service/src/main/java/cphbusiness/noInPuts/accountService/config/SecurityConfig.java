99adb6970ef721443b4ad885420c2a5d867de585
Benjamin Sommervoll <puixdk@gmail.com>
2023-11-03 14:46:42 +0100

    Added config for security (WIP

---
+++ account_service/src/main/java/cphbusiness/noInPuts/accountService/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package cphbusiness.noInPuts.accountService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
