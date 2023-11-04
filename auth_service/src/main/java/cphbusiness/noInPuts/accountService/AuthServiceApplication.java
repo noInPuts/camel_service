7def3aec64324cdc15c7e8dc800a1992789891d2
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-11-04 11:17:26 +0100

    Rename service to auth service

--- auth_service/src/main/java/cphbusiness/noInPuts/accountService/AuthServiceApplication.java
+++ auth_service/src/main/java/cphbusiness/noInPuts/accountService/AuthServiceApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountServiceApplication {
+public class AuthServiceApplication {
 
 	// TODO: Automated acceptance tests
 	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
