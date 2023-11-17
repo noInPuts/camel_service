9d3762f8f9486084d112ec09e4d5f3d7cc2ebe10
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-11-17 01:03:14 +0100

    get user endpoint

--- user_service/src/test/java/cphbusiness/noInPuts/userService/controller/UserControllerIntegrationTests.java
+++ user_service/src/test/java/cphbusiness/noInPuts/userService/controller/UserControllerIntegrationTests.java
@@ -1,10 +1,13 @@
 package cphbusiness.noInPuts.userService.controller;
 
+import cphbusiness.noInPuts.userService.model.User;
+import cphbusiness.noInPuts.userService.repository.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.Cookie;
 import net.datafaker.Faker;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,7 +22,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -33,8 +38,37 @@ public class UserControllerIntegrationTests {
     @Value("${jwt.secret}")
     private String pKey;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     public void createUser() throws Exception {
+        // Creating faker object
+        Faker faker = new Faker();
+
+        // Creating a map with the data we want to send in the request
+        Map<String, Object> jsonRequestMap = new HashMap<>();
+        jsonRequestMap.put("username", "my_test_user");
+        jsonRequestMap.put("id", 1L);
+        jsonRequestMap.put("name", faker.name().fullName());
+        jsonRequestMap.put("address", faker.address().fullAddress());
+        jsonRequestMap.put("phoneNumber", faker.phoneNumber().cellPhone());
+        jsonRequestMap.put("email", faker.internet().emailAddress());
+
+        // Converting the map to a JSON object
+        JSONObject jsonObject = new JSONObject(jsonRequestMap);
+        String jsonRequestData = jsonObject.toString();
+
+        // Getting cookie
+        Cookie cookie = getJwtTokenCookie();
+
+        this.mockMvc.perform(post("/user/create").content(jsonRequestData).contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8").cookie(cookie))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void createUserWithWrongId() throws Exception {
         // Creating faker object
         Faker faker = new Faker();
 
@@ -55,7 +89,7 @@ public class UserControllerIntegrationTests {
         SecretKey key = Keys.hmacShaKeyFor(pKey.getBytes());
         String jwtToken = Jwts.builder()
                 .header()
-                .add("id", 1L)
+                .add("id", 2L)
                 .add("username", "user")
                 .add("role", "admin")
                 .and()
@@ -67,7 +101,39 @@ public class UserControllerIntegrationTests {
         Cookie cookie = new Cookie("jwt-token", jwtToken);
 
         this.mockMvc.perform(post("/user/create").content(jsonRequestData).contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8").cookie(cookie))
-                .andExpect(status().isOk());
+                .andExpect(status().isForbidden());
     }
 
+    @Test
+    public void getUser() throws Exception {
+        Faker faker = new Faker();
+        User user = new User(1L, faker.name().fullName(), faker.internet().emailAddress(), faker.phoneNumber().cellPhone(), faker.address().fullAddress());
+        userRepository.save(user);
+
+        // Getting cookie
+        Cookie cookie = getJwtTokenCookie();
+
+        this.mockMvc.perform(get("/user").cookie(cookie))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("{\"id\": "+ user.getId() +",\"name\":\"" + user.getName() + "\",\"email\":\"" + user.getEmail() + "\",\"phoneNumber\":\"" + user.getPhoneNumber() + "\",\"address\":\"" + user.getAddress() + "\"}"));
+    }
+
+    private Cookie getJwtTokenCookie() {
+        // Creating a cookie with a jwt token
+        SecretKey key = Keys.hmacShaKeyFor(pKey.getBytes());
+        String jwtToken = Jwts.builder()
+                .header()
+                .add("id", 1L)
+                .add("username", "user")
+                .add("role", "admin")
+                .and()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                // Expires after 2 days of issue
+                .expiration(new Date(System.currentTimeMillis() + 2 * 24 * 60 * 60 * 1000))
+                .signWith(key)
+                .compact();
+
+        return new Cookie("jwt-token", jwtToken);
+    }
 }
