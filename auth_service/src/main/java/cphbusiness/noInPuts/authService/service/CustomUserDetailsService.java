26270b4e5b336d7be655002a24b07e1e911aac89
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-11-18 05:06:37 +0100

    Removed Spring Security (Using own implementation)

--- auth_service/src/main/java/cphbusiness/noInPuts/authService/service/CustomUserDetailsService.java
+++
@@ -1,6 +0,0 @@
-package cphbusiness.noInPuts.authService.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface CustomUserDetailsService extends UserDetailsService {
-}
