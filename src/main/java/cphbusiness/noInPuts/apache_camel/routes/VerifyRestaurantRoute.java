cbe1829b57604dc14466ac45d820a76432cd8654
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-12-15 15:35:28 +0100

    Test folder structure

--- src/main/java/cphbusiness/noInPuts/apache_camel/routes/VerifyRestaurantRoute.java
+++ src/main/java/cphbusiness/noInPuts/apache_camel/routes/VerifyRestaurantRoute.java
@@ -1,19 +1,25 @@
 package cphbusiness.noInPuts.apache_camel.routes;
 
-import org.apache.camel.LoggingLevel;
+import org.apache.camel.ExchangePattern;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-// TODO: Comments/Documentation
 @Component
-public class RabbitCamelRoutes extends RouteBuilder {
-
-    // TODO: Create multiple files for each route
-    // TODO: Give route a name (Displays route1 in logs)
+public class VerifyRestaurantRoute extends RouteBuilder {
     @Override
     public void configure() {
+        // gRPC -> REST
+
+        /*
+        // Kafka
+        from("kafka:getRestaurant?brokers=localhost:9092")
+                .setExchangePattern(ExchangePattern.InOut)
+                .log("Restaurant is verified: ${body}");
+
+        // RabbitMQ
         from("spring-rabbitmq:default?queues=userCreated&routingKey=userCreated")
                 .log(LoggingLevel.INFO, "User is created: ${body}")
                 .to("spring-rabbitmq:default?queues=persistUserInUserService&routingKey=persistUserInUserService");
+        */
     }
 }
